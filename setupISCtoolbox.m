8a7c26d33f940daee95add628232c5b5478b4727
tomgarth <152499843+tomgarth@users.noreply.github.com>
2025-11-17 14:28:58 +0000

    Update setupISCtoolbox.m
    Add more paths

--- setupISCtoolbox.m
+++ setupISCtoolbox.m
@@ -1,11 +1,12 @@
 function setupISCtoolbox()
 
 folder = fileparts(mfilename("fullpath"));
-addpath( fullfile(folder,"Functions") )
-addpath( fullfile(folder,"Functions/focalmech") )
-addpath( fullfile(folder,"Data") )
-addpath( fullfile(folder,"Data/Downloaded_ISC_Data") )
-addpath( fullfile(folder,"Data/Example_ISC_Data") )
+addpath( fullfile(folder,"Functions") );
+addpath( fullfile(folder,"Functions/focalmech") );
+addpath( fullfile(folder,"Functions/altmany-export_fig-3/") );
+addpath( fullfile(folder,"Data") );
+addpath( fullfile(folder,"Data/Downloaded_ISC_Data") );
+addpath( fullfile(folder,"Data/Example_ISC_Data") );
 
 addpath( folder )
 % addpath Functions/
