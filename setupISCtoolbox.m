cf266d86ad8d85e0135ea8b6f2e32b0f3c65ccc3
tomgarth <152499843+tomgarth@users.noreply.github.com>
2025-11-17 14:13:46 +0000

    Update setupISCtoolbox.m
    To add paths to focalmech toolbox

--- setupISCtoolbox.m
+++ setupISCtoolbox.m
@@ -2,6 +2,7 @@ function setupISCtoolbox()
 
 folder = fileparts(mfilename("fullpath"));
 addpath( fullfile(folder,"Functions") )
+addpath( fullfile(folder,"Functions/focalmech") )
 addpath( fullfile(folder,"Data") )
 addpath( fullfile(folder,"Data/Downloaded_ISC_Data") )
 addpath( fullfile(folder,"Data/Example_ISC_Data") )
